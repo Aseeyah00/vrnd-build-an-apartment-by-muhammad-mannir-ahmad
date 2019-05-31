dbeb584ada7755c1ec51a99dd324b759c4904190
Mannir <manniru@gmail.com>
2019-05-31 10:37:43 +0100

    Fixed Reviews

---
+++ Build an Apartment/Assets/UdacityVR/Scripts/MannirAnimate.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MannirAnimate : MonoBehaviour {
+
+	GameObject go;
+	float angle = 360.0f; // Degree per time unit
+	float time = 1.0f; // Time unit in sec
+	Vector3 axis = Vector3.up; // Rotation axis, here it the yaw axis
+
+	// Use this for initialization
+	void Start () {
+
+
+		
+	}
+
+	// Update is called once per frame
+	void Update () {
+//		transform.Translate(0, -2.5f * Time.deltaTime, 0, Space.World);
+		transform.Rotate(0, angle * Time.deltaTime / time, 0, Space.World);
+//		go.GetComponent<Transform>().RotateAround(Vector3.zero, axis, angle * Time.deltaTime / time);
+		
+	}
+}
--- Build an Apartment/Assets/UdacityVR/Scripts/TriggerAnimation.cs
+++ Build an Apartment/Assets/UdacityVR/Scripts/TriggerAnimation.cs
@@ -1,4 +1,27 @@
-﻿using UnityEngine;
+﻿//using UnityEngine;
+//using System.Collections;
+//
+///// <summary>
+///// The TriggerAnimation class activates a transition whenever the Cardboard button is pressed (or the screen touched).
+///// </summary>
+//public class TriggerAnimation : MonoBehaviour
+//{
+//	[Tooltip ("The name of the Animator trigger parameter")]
+//	public string animationName;
+//	[Tooltip ("The Animator component on this gameobject")]
+//	public Animator stateMachine;
+//
+//	void Update ()
+//	{
+//		// If the player pressed the cardboard button (or touched the screen), set the trigger parameter to active (until it has been used in a transition)
+//		if (Input.GetMouseButtonDown (0)) {
+//			stateMachine.SetTrigger (animationName);
+//		}
+//	}
+//}
+
+
+using UnityEngine;
 using System.Collections;
 
 /// <summary>
@@ -6,16 +29,32 @@ using System.Collections;
 /// </summary>
 public class TriggerAnimation : MonoBehaviour
 {
-	[Tooltip ("The name of the Animator trigger parameter")]
-	public string animationName;
 	[Tooltip ("The Animator component on this gameobject")]
-	public Animator stateMachine;
+	public Animator animator;
+	[Tooltip ("The name of the Animator trigger parameter")]
+	public string triggerName;
 
 	void Update ()
 	{
 		// If the player pressed the cardboard button (or touched the screen), set the trigger parameter to active (until it has been used in a transition)
 		if (Input.GetMouseButtonDown (0)) {
-			stateMachine.SetTrigger (animationName);
+			if (animator == null) {
+				Debug.LogWarning("Please add an animator to your " + gameObject.name);
+				return;
+			}
+			if (triggerName == null) {
+				Debug.LogWarning("What's the trigger parameter name you want to call? Please add it to the TriggerAnimation script on your " + gameObject.name);
+				return;
+			}
+			animator.SetTrigger (triggerName);
+		}
+	}
+
+	private void Start()
+	{
+		animator = GetComponent<Animator>();
+		if(animator == null) {
+			Debug.LogWarning("Please add an animator to your " + gameObject.name);
 		}
 	}
 }
